01fec1abd90c2a78ebb5451dc06e3c9ee7dccd00
levancuon <levancuongprovip@gmail.com>
2024-04-04 00:12:05 +0700

    asdqwmko

---
+++ baiTap_Hien-thi-loi-chao/src/HienThiLoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("nhap ten");
+        String name = input.nextLine();
+        System.out.println("xin chao "+name);
+    }
+}
