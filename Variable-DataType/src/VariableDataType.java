710fbfd9ab930b9fea8baae7f746b93194790933
levancuon <levancuongprovip@gmail.com>
2024-04-03 21:53:22 +0700

    lan2

---
+++ Variable-DataType/src/VariableDataType.java
@@ -0,0 +1,16 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10; //khai báo biến i là kiểu init có giá trị là 10
+        float f=20.5f; // khai báo biến f có kiểu là float có giá trị là 20.5
+        double d = 20.5;
+        boolean b = true;
+        char c = 'c';
+        String s = "Hà Nội";
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+    }
+}
