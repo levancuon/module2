e37f506d6e8a8b5ab95e615c41b063f3eea0aae7
levancuon <levancuongprovip@gmail.com>
2024-04-17 14:14:50 +0700

    adwac

---
+++ thucHanh_StaticMethod/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student.change(); //calling change method
+
+        //creating objects
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+
+        //calling display method
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
