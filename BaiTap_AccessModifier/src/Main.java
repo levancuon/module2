2eac2b2799044af7bb437463156d7008a57faf0c
levancuon <levancuongprovip@gmail.com>
2024-04-17 15:24:11 +0700

    asdniqw

---
+++ BaiTap_AccessModifier/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+    Circle circle=new Circle(5);
+        System.out.println("dien tich = "+circle.getArea());
+        System.out.println("Ban kinh = "+circle.getRadius());
+    }
+}
