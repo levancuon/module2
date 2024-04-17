6e816f809306b3027a64cca641a7c60e9dc1919f
levancuon <levancuongprovip@gmail.com>
2024-04-17 15:32:54 +0700

    qwni

---
+++ BaiTap_XayDungLopChiGhiTrongJava/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student student= new Student();
+        student.setName("HUY");
+        student.setClasses("H20");
+        System.out.println(student.name);
+        System.out.println(student.classes);
+    }
+}
---
+++ BaiTap_XayDungLopChiGhiTrongJava/src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    public String name = "John";
+    public String classes="C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
