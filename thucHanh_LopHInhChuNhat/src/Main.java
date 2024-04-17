6b727e66af965eb9012ddbb8f923a58e19f0a74b
levancuon <levancuongprovip@gmail.com>
2024-04-17 12:41:46 +0700

    asdqiwun

--- thucHanh_LopHInhChuNhat/src/Main.java
+++ thucHanh_LopHInhChuNhat/src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Rectangle rectangle=new Rectangle(4,5);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("nhap width");
+        double width = input.nextDouble();
+        System.out.println("nhap height");
+        double height = input.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
         System.out.println(rectangle);
     }
 }
